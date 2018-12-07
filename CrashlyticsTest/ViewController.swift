70fc2267dfd51e59ae9c1046245874cdb8cca924
山中 <makoto.y@equalplus.jp>
2018-12-07 17:19:52 +0900

    クラッシュ実行コード追加

--- CrashlyticsTest/ViewController.swift
+++ CrashlyticsTest/ViewController.swift
@@ -2,8 +2,25 @@
 //	Copyright © 2018 Makoto Yamanaka. All rights reserved.
 //
 import UIKit
+import Crashlytics
 
 class ViewController: UIViewController {
+	
+	//────────────────────
+	// MARK: ■ Actions
+	//────────────────────
+	
+	@IBAction func crashlyticsCrash() {
+		Crashlytics.sharedInstance().crash()
+	}
+	
+	@IBAction func crashlyticsThrowException() {
+		Crashlytics.sharedInstance().throwException()
+	}
+	
+	//────────────────────
+	// MARK: ■ UIViewController override
+	//────────────────────
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
