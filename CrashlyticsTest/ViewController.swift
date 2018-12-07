73f25ed7f8e867dde9d2e297be38d6d78e7bb40b
山中 <makoto.y@equalplus.jp>
2018-12-07 16:20:30 +0900

    add iOS Single View project

---
+++ CrashlyticsTest/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//	Copyright © 2018 Makoto Yamanaka. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+}
